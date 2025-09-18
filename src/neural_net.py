Fixed a bug in the neural net code for the loss computation to use the loss from all epochs not just the final one, fixed train_on_batch in NeuralNet to slice the data  and cleaned up some code across the code base

src/neural_net.py
@@ -1,5 +1,3 @@
-# NeuralNet.py
-from math import sqrt
 import numpy as np
 from mpi4py import MPI
 from processing.mpi_utils import rank, size, comm
@@ -8,18 +6,27 @@ from log.log import log_test_rmse, log_training_metrics
 
 
 class NeuralNet:
-    def __init__(self, input_dim, hidden_dim, learning_rate, activation_fn, seed):
+    """
+    Defining a structure for the NeuralNet model with the parameters -
+    input_dim, hidden_dim, learning_rate, activation_function, num_processes and input seed.
+
+    We initialize the starting weights based on the activation function to ensure faster
+    convergence following https://www.deeplearning.ai/ai-notes/initialization/index.html
+    """
+    def __init__(self, input_dim, hidden_dim, learning_rate, activation_fn, num_processes, seed):
         # randomly initialize weights by rank
         np.random.seed(seed + rank)
         self.learning_rate = learning_rate
         self.activation_fn = activation_fn
+        self.num_processes = num_processes
+
         # initialize weights based on activation function
         if activation_fn == "relu": 
             # He initialization (good for ReLU) 
             self.W1 = np.random.randn(input_dim, hidden_dim) * np.sqrt(2.0 / input_dim) 
             self.W2 = np.random.randn(hidden_dim, 1) * np.sqrt(2.0 / hidden_dim) 
         else: 
-            # default to Xavier if linear, sigmoid, tanh 
+            # default to Xavier initialization if not ReLu i.e. linear, sigmoid, tanh 
             self.W1 = np.random.randn(input_dim, hidden_dim) / np.sqrt(input_dim) 
             self.W2 = np.random.randn(hidden_dim, 1) / np.sqrt(hidden_dim)
         self.b1 = np.zeros((1, hidden_dim))
@@ -63,34 +70,54 @@ class NeuralNet:
 
 def train_on_batch(model, X_batch, y_batch):
     """
-    Distributed SGD step: forward, backward, MPI Allreduce, apply update
+    For a given batch of training data, we do the following steps:
+     - calculate y_pred for train data (batch) using forward propagation, 
+     - backpropagate error between y_pred and y_train (batch) to get the local gradients 
+     - execute MPI allreduce to sum the local gradients to get the global gradients, 
+     - apply gradient updates to adjust the weights of the model
+     - compute and return the local sum of squared errors for the global train RMSE calculation in train()
     """
-    # Forward
-    y_pred = model.forward(X_batch)
+    batch_size = X_batch.shape[0]
+    rank_batch_size = int(np.ceil(batch_size / size))  # splitting nearly equally across ranks
 
-    # Compute local gradients
-    local_gradients = model.backward(X_batch, y_batch, y_pred)
+    # Determine slice for this rank
+    begin_index_local = rank * rank_batch_size
+    end_index_local = min((rank + 1) * rank_batch_size, batch_size)
+    X_local = X_batch[begin_index_local:end_index_local]
+    y_local = y_batch[begin_index_local:end_index_local]
 
-    # aggregate gradients across processes
-    global_gradients = [np.zeros_like(local_gradient) for local_gradient in local_gradients]
-    for local_gradient, global_gradient in zip(local_gradients, global_gradients):
-        comm.Allreduce(local_gradient, global_gradient, op=MPI.SUM)
-    global_gradients = [global_gradient / size for global_gradient in global_gradients]
+    if X_local.shape[0] > 0:
+        # Forward propagation on local slice
+        y_pred_local = model.forward(X_local)
+        # Compute local gradients
+        local_gradients = model.backward(X_local, y_local, y_pred_local)
+    else:
+        # Skipping forward and backward propagation if no data for this rank
+        local_gradients = [
+            np.zeros_like(model.W1),
+            np.zeros_like(model.b1),
+            np.zeros_like(model.W2),
+            np.zeros_like(model.b2)
+        ]
+        y_pred_local = np.zeros((0,1))
+
+    # Aggregate gradients across ranks (average)
+    global_gradients = [comm.allreduce(g, op=MPI.SUM) / size for g in local_gradients]
 
     # update the weights of the model using the average global gradients
     model.apply_gradients(global_gradients)
 
-    # Compute loss (MSE)
-    loss = np.mean((y_pred - y_batch.reshape(-1, 1)) ** 2)
-    return loss
-
+    # Compute local sum of squared errors for RMSE calculation
+    local_sum_of_squares = np.sum((y_pred_local - y_local.reshape(-1, 1)) ** 2)
+    return local_sum_of_squares
 
 def train(model, X_train, y_train, epochs, batch_size, seed):
     iteration = 0 
-    loss = 0
+    total_sum_of_squares = 0.0
+    total_count = 0
 
     for epoch in range(epochs):
-        # Shuffle training data (all ranks must use same permutation)
+        # shuffle training data uniformly across all processes
         indices = np.arange(X_train.shape[0])
         if rank == 0:
             np.random.seed(seed + epoch)
@@ -103,20 +130,32 @@ def train(model, X_train, y_train, epochs, batch_size, seed):
             X_batch = X_train[i:i+batch_size]
             y_batch = y_train[i:i+batch_size]
 
-            # Train on batch (distributed)
-            loss = train_on_batch(model, X_batch, y_batch)
+            # Train on batch (slicing the batch across processes within train_on_batch)
+            local_sum_of_squares = train_on_batch(model, X_batch, y_batch)
 
             # Increment iteration counter
             iteration += 1
 
+            global_sum_of_square_errors = comm.allreduce(local_sum_of_squares, op=MPI.SUM)
+            global_count = comm.allreduce(len(y_batch), op=MPI.SUM)
+            total_sum_of_squares += global_sum_of_square_errors
+            total_count += global_count
+            batch_loss = global_sum_of_square_errors / global_count
+
             # Log iteration, epoch, params, and loss (only rank 0 writes)
             if rank == 0:
-                log_training_metrics(iteration, epoch+1, batch_size, model.activation_fn, model.learning_rate, loss)
-    return sqrt(loss)
+                log_training_metrics(iteration, epoch+1, batch_size, model.activation_fn, model.learning_rate, model.num_processes, batch_loss)
+
+
+    # returning train RMSE as sqrt(training loss)
+    return np.sqrt(total_sum_of_squares / total_count)
 
 
 def evaluate(model, X_test, y_test):
-    """Distributed RMSE evaluation"""
+    """The evaluation function does the following:
+    - performs forward propagation to get y_pred for the feature data
+    - computes the RMSE on the test data using y_pred and y_test.
+    Returns the test RMSE"""
     # forward pass to get predictions
     y_pred = model.forward(X_test).flatten()
 
@@ -124,7 +163,7 @@ def evaluate(model, X_test, y_test):
     local_sum_of_squares = float(np.sum((y_pred - y_test) ** 2))
     local_count = len(y_test)
 
-    # Allreduce to get global sum of squared errors and count
+    # allreduce to get global sum of squared errors and count
     global_sum_of_squares = comm.allreduce(local_sum_of_squares, op=MPI.SUM)
     global_count = comm.allreduce(local_count, op=MPI.SUM)
 
@@ -150,38 +189,40 @@ def execute_model(model, X_train, y_train, X_test, y_test,
     train_rmse = train(model, X_train, y_train, epochs, batch_size, seed)
 
     train_end = MPI.Wtime()
-    eval_start = MPI.Wtime()
+    test_start = MPI.Wtime()
 
     test_rmse = evaluate(model, X_test, y_test)
 
-    eval_end = MPI.Wtime()
+    test_end = MPI.Wtime()
     total_end = MPI.Wtime()
 
     # collect timings across ranks
     local_train_time = train_end - train_start
-    local_eval_time = eval_end - eval_start
+    local_test_time = test_end - test_start
     local_total_time = total_end - total_start
 
     # determine the slowest time among all ranks
     train_time_max = comm.allreduce(local_train_time, op=MPI.MAX)
-    eval_time_max = comm.allreduce(local_eval_time, op=MPI.MAX)
+    test_time_max = comm.allreduce(local_test_time, op=MPI.MAX)
     total_time_max = comm.allreduce(local_total_time, op=MPI.MAX)
 
     # determine the average time across all ranks
     train_time_avg = comm.allreduce(local_train_time, op=MPI.SUM) / size
-    eval_time_avg = comm.allreduce(local_eval_time, op=MPI.SUM) / size
+    test_time_avg = comm.allreduce(local_test_time, op=MPI.SUM) / size
     total_time_avg = comm.allreduce(local_total_time, op=MPI.SUM) / size
 
     # Log train and test RMSE
     if rank == 0:
         log_test_rmse(
-            epochs, batch_size, model.activation_fn, model.learning_rate,train_rmse, test_rmse,
+            model.num_processes, epochs, batch_size, model.activation_fn, model.learning_rate,
+            train_rmse, test_rmse, train_time_max, train_time_avg, test_time_max, test_time_avg, 
+            total_time_max, total_time_avg,
             logfile="../logs/normalization_fix/train_test_rmse.csv"
         )
         print(f"[Epoch {epochs}] Train RMSE: {train_rmse:.4f} Test RMSE: {test_rmse:.4f}")
         print(f"[Timing Summary] "
               f"Train: {train_time_max:.3f}s (max) / {train_time_avg:.3f}s (avg)  "
-              f"Eval: {eval_time_max:.3f}s (max) / {eval_time_avg:.3f}s (avg)  "
+              f"Test: {test_time_max:.3f}s (max) / {test_time_avg:.3f}s (avg)  "
               f"Total: {total_time_max:.3f}s (max) / {total_time_avg:.3f}s (avg)")
 
     return train_rmse, test_rmse

src/processing/io.py
@@ -34,8 +34,8 @@ class DatasetLoader:
         # Process with rank 0 broadcasts the row count to all processes
         num_rows_total = comm.bcast(n_rows, root=0)
 
-        # Partition rows across ranks almost equally (as num_rows_total may not be divisible by size)
-        rows_per_rank = num_rows_total // size
+        # Partition rows across ranks almost equally
+        rows_per_rank = int(np.ceil(num_rows_total / size))
         begin_index_local = rank * rows_per_rank
         end_index_local = (rank + 1) * rows_per_rank if rank < size - 1 else num_rows_total
         num_rows_local = end_index_local - begin_index_local
@@ -73,8 +73,8 @@ class DatasetLoader:
             print(f"[Rank {rank}] has split test-train data with {train_size} - {test_size} split")
 
         
-        # Although not the case with the current dataset as it has only 3% missing values,
-        # this else case is to handle the edge case of a process that gets no data after preprocessing
+        # Although not the case with the current dataset has no missing values,
+        # this else case is to handle the edge case of a process that gets no data
         else:
             X_train, X_test = np.empty((0, 0)), np.empty((0, 0))
             y_train, y_test = np.empty((0,)), np.empty((0,))
@@ -93,13 +93,13 @@ class DatasetLoader:
             return sum(1 for _ in f) - 1  # subtract header
 
     @staticmethod
-    def split_test_train(X, y, test_ratio, random_state):
+    def split_test_train(X, y, test_ratio, random_seed):
         '''
         Split the data into test and train sets given a test ratio and a random seed
-        Returns X_train, y_train, X_test, y_test in that order
+        Returns X_train, y_train, X_test, y_test, train_size, test_size
         '''
         # setting a random seed to make the shuffling deterministic
-        np.random.seed(random_state)
+        np.random.seed(random_seed)
 
         num_samples = X.shape[0]
         indices = np.arange(num_samples)

src/utils/activation_functions.py
@@ -16,4 +16,4 @@ def sigmoid_grad(z):
 def tanh(x): return np.tanh(x)
 def tanh_grad(z): return 1 - np.tanh(z) ** 2
 
-#TODO try using softmax function, if it has the same issue as sigmoid, we need approximations
+#TODO add more activation functions here and in the map in constants.py - softmax